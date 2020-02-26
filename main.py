06cf1649f6f7cdfd4844ef40131652ac2b3052af
HyperHamster535 <hyperhamster534@outlook.com>
2020-02-26 21:37:18 +0000

    added propaganda

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import csv
 import random
 
-rawJokes = csv.reader(open('jokes.csv', 'r'))
+rawJokes = csv.reader(open('assets/jokes.csv', 'r'))
 jokes = sum([i for i in rawJokes],[])
 
 TOKEN = 'NjgyMzI5OTA1ODk5NjM0NzMz.XlbbfQ.b5nto8lGAwhXeNxWjLCxoxcgiaM'
@@ -14,11 +14,16 @@ client = discord.Client()
 
 async def on_message(message):
      if message.author == client.user:
-        print(" ")
         return
 
-     if message.content == "$joke":
+     if message.content.lower() == "$joke":
           await message.channel.send(random.choice(jokes))
+
+     if message.content.lower() == "$propaganda":
+          for i in range(15):
+               await message.channel.send("@everyone THIS SERVER HAS BEEN CLAIMED AS A COLONY OF THE DEMOCRATIC REPUBLIC OF HEINZ")
+               await message.channel.send(file=discord.File('assets/flag.png'))
+               await message.channel.send("Join us: https://discord.gg/arvVft4")
 
 @client.event
 async def on_ready():
