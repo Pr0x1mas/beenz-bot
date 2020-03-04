df3a6434f304b467bfbc499c180c116ba5485cba
HyperHamster535 <hyperhamster534@outlook.com>
2020-03-04 21:46:38 +0000

    added colonial flag and updated help command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #       ===beenz-bot v1.2===
 #   ===Copyright 2020 Pr0x1mas===
 
+sysversion = "1.2"
+
 print("Loading beenzbot...")
 
 import discord
@@ -21,9 +23,10 @@ client = discord.Client()
 async def on_message(message):
      if message.author == client.user: #prevent bot replying to itself
           return
-     
+
+     #--Help command---
      if message.content.lower() == "$help":
-          await message.channel.send("```beenz-bot Version 1.1 \n \n Help \n \n $meme - send a meme from r/dankmemes \n \n $beans - sends a cursed bean image from r/beansinstrangeplaces```")
+          await message.channel.send("```beenz-bot Version " + sysversion + " \n \n (c) 2020 @Pr0x1mas#0632, with help from @Alexander Litvinenko#2894 and @Reaper_Vortex#4130 \n \n Help \n \n $meme - send a meme from r/dankmemes \n \n $beans - sends a cursed bean image from r/beansinstrangeplaces```")
                
      #--Fake meme feature--
      if message.content.lower() == "$meme":
