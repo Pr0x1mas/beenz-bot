b1bdbe79a22336c7473aeac800f358e64d5e7b07
HyperHamster535 <hyperhamster534@outlook.com>
2020-03-12 17:13:33 +0000

    new version number

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
 #                     ===beenz-bot v2.0===
 #                       ===globals.py===
 #  ======Copyright 2020 Pr0x1mas, TheProgramableTurtle======
-sysversion = "2.0"
+sysversion = "2.2"
 originalname = None
