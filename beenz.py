6316514fb4267c008a4a1cb6dc0c5a09bc26813f
HyperHamster535 <hyperhamster534@outlook.com>
2020-04-06 10:41:08 +0100

    opus lib now locally hosted to hopefully allow the bot to play audio from heroku

--- beenz.py
+++ beenz.py
@@ -14,6 +14,14 @@ import inspect
 import youtube_dl
 import asyncio
 
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    discord.opus.load_opus('libopus-0.dll')
+
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
