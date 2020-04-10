f5b9572facb3405caf8fa90084adba41f39b6d50
Pr0x1mas <50422789+Pr0x1mas@users.noreply.github.com>
2020-04-11 00:59:28 +0100

    removed some unneeded code and update ver num

--- globals.py
+++ globals.py
@@ -1,8 +1,7 @@
 #                     ===beenz-bot v2.4.2b===
 #                       ===globals.py===
 #  ======Copyright 2020 Pr0x1mas, TheProgramableTurtle======
-sysversion = "2.4.2b"
-originalname = ""
+sysversion = "2.4.2"
 OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
 
