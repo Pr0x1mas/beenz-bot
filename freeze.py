162ae57cea7d7a3ad6eb0d9d1e2935836fa6842b
Mika Jones <mlljones@icloud.com>
2020-04-10 14:36:35 +0100

    Fixed installer

--- freeze.py
+++ freeze.py
@@ -1,8 +1,10 @@
-import sys
+#                     ===beenz-bot v2.5b===
+#                       ===freeze.py===
+#  ======Copyright 2020 Pr0x1mas, TheProgramableTurtle======
+
 from cx_Freeze import setup, Executable
 
-
-packages = ["os", "time", "random", "inspect", "asyncio", "io"]
+packages = ["os", "time", "random", "inspect", "asyncio", "io", "pathlib"]
 with open("requirements", "r") as requirements:
     for line in requirements:
         packages.append(line.rstrip())
@@ -10,18 +12,14 @@ with open("requirements", "r") as requirements:
 
 build_exe_options = {"packages": packages}
 bdist_msi_options = {"initial_target_dir": "C:\Program Files\metalgearrape",
-                     "add_to_path": True,
                      "all_users": True}
 print(packages)
-"""
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-"""
 
 setup(  name = "metalgearrape",
         version = "2.5b",
         description = "Server software for metalgearrape discord bot",
         options = {"build_exe": build_exe_options,
                    "bdist_msi": bdist_msi_options},
-        executables = [Executable("metalgearrape.py")])
+        executables = [Executable("metalgearrape.py",
+                                  shortcutName="metalgearrape",
+                                  shortcutDir="StartMenuFolder")])
--- metalgearrape.py
+++ metalgearrape.py
@@ -1,11 +1,11 @@
-#                     ===beenz-bot v2.4===
-#                        ===metalgearrape.py===
+#                    ===beenz-bot v2.5b===
+#                   ===metalgearrape.py===
 #  ======Copyright 2020 Pr0x1mas, TheProgramableTurtle======
+
 print("Loading beenzbot...")
 
 import beenz
 from globals import *
-from discord.ext import commands as cmd
 # ---Setup connection to bot---
 TOKEN = "***REMOVED***"
 
