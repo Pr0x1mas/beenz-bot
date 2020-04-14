5b4a58b643294d3e600f878bdb7cfaa33e609449
HyperHamster535 <hyperhamster534@outlook.com>
2020-04-14 12:36:31 +0100

    removed a load of stuff we don't need and did some restructuring

--- src/main.py
+++ src/main.py
@@ -5,9 +5,12 @@
 print("Loading beenzbot...")
 
 import beenz
+import os
 from globals import *
+from dotenv import load_dotenv
+load_dotenv(dotenv_path = os.path.join(os.path.dirname(__file__), '.env'))
 # ---Setup connection to bot---
-TOKEN = "Njg5NTczMzUwMDg3MDY1NjQ4.XnE7GQ.Oy_RUZXdYu_IuemhXJt-Zk0_IjE"
+TOKEN = os.getenv("TOKEN")
 
 client = beenz.Bot(sysversion)
 
